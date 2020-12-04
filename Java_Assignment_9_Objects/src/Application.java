b046300e7351823c5928843bb9954b2fcdf17f96
jeffery graham <jeffery.lemar.graham@gmail.com>
2020-12-03 21:35:40 -0500

    Java_Assignment_9_Objects exercises

---
+++ Java_Assignment_9_Objects/src/Application.java
@@ -0,0 +1,11 @@
+
+public class Application {
+		
+	public static void main(String[] args) {
+		MyObject one = new MyObject(511L);
+		MyObject two = new MyObject(511L);
+		
+		System.out.println(one.equals(two));
+	}
+}
+	
